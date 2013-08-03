bceb2bf9c1e6cb26880e97a20967ae26faed5e48
Gregory Tkach <gregory.tkach@gmail.com>
2013-08-03 08:18:38 +0300

    merge index and footer

--- page2/script/main.js
+++ page2/script/main.js
@@ -5,3 +5,19 @@
  * Time: 10:09 AM
  * To change this template use File | Settings | File Templates.
  */
+
+function onLoad()
+{
+    getSkypeStatus('alexyanovskyschool', 'smallicon', 'call', '#skype_icon');
+
+    setTimeout(loadAssetsAsync, 1000);
+}
+
+
+function getImageNames()
+{
+    return         [
+        '../assets/section0/button_registration_hover.png',
+        '../assets/section3/section_3_button_hover.png'
+    ];
+}
